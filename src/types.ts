b890afee1506ac6e691b7d20bbc99d7963753d0f
Noriskky <Noriskky@proton.me>
2025-03-07 07:19:40 +0100

    Refactor types for video metadata and transcript items

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,11 @@
-/**
- * Internal types for the Library.
- * Re-export them in `mod.ts` if you'd like to expose them to consumers as well.
- */
-
-/**
- * Configuration options for the Lib class
- */
-interface LibConfig {
-  [key: string]: unknown
+export interface TranscriptItem {
+  text: string;
+  offset: number;
+  duration: number;
+  [key: string]: any;
 }
 
-type CrudOperation = {
-  [key: string]: unknown
+export interface VideoMetadata {
+  title: string;
+  creator: string;
 }
-
-type LibRequest = CrudOperation
-type LibResult = CrudOperation
-
-export type { LibConfig, LibRequest, LibResult }
